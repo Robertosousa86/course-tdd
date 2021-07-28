0fc8323ad3aad2e4024eda34ab1f0385bbfef6ee
Roberto Sousa <robertosousa186@gmail.com>
2021-07-28 04:12:26 -0300

    Adding describe for tests

--- __tests__/UserRegister.spec.js
+++ __tests__/UserRegister.spec.js
@@ -1,30 +1,32 @@
 const app = require('../app');
 const request = require('supertest');
 
-it('should be returns 200 OK when singup request is valid', (done) => {
-  request(app)
-    .post('/api/1.0/users')
-    .send({
-      username: 'user1',
-      email: 'user1@mail.com',
-      password: 'P4ssword',
-    })
-    .then((res) => {
-      expect(res.status).toBe(200);
-      done();
-    });
-});
+describe('User Registration', () => {
+  it('should be returns 200 OK when singup request is valid', (done) => {
+    request(app)
+      .post('/api/1.0/users')
+      .send({
+        username: 'user1',
+        email: 'user1@mail.com',
+        password: 'P4ssword',
+      })
+      .then((res) => {
+        expect(res.status).toBe(200);
+        done();
+      });
+  });
 
-it('should be returns sucess message when singup request is valid', (done) => {
-  request(app)
-    .post('/api/1.0/users')
-    .send({
-      username: 'user1',
-      email: 'user1@mail.com',
-      password: 'P4ssword',
-    })
-    .then((res) => {
-      expect(res.body.message).toBe('User created.');
-      done();
-    });
+  it('should be returns sucess message when singup request is valid', (done) => {
+    request(app)
+      .post('/api/1.0/users')
+      .send({
+        username: 'user1',
+        email: 'user1@mail.com',
+        password: 'P4ssword',
+      })
+      .then((res) => {
+        expect(res.body.message).toBe('User created.');
+        done();
+      });
+  });
 });
