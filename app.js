86d4d610a41044827d27e2646a029736ff174832
Roberto Sousa <robertosousa186@gmail.com>
2021-07-28 04:07:54 -0300

    Adding body object with sucess message

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.post('/api/1.0/users', (req, res) => {
-  return res.send();
+  return res.send({ message: 'User created.' });
 });
 
 module.exports = app;
