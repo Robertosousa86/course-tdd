7b3fa6265dc99aea8d13d0cdebdcb360c4e5a83e
Roberto Sousa <robertosousa186@gmail.com>
2021-07-28 04:06:21 -0300

    Creating test for sucess message

--- __tests__/UserRegister.spec.js
+++ __tests__/UserRegister.spec.js
@@ -14,3 +14,17 @@ it('should be returns 200 OK when singup request is valid', (done) => {
       done();
     });
 });
+
+it('should be returns sucess message when singup request is valid', (done) => {
+  request(app)
+    .post('/api/1.0/users')
+    .send({
+      username: 'user1',
+      email: 'user1@mail.com',
+      password: 'P4ssword',
+    })
+    .then((res) => {
+      expect(res.body.message).toBe('User created.');
+      done();
+    });
+});
