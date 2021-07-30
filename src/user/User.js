089157ca4c1a34977b63d8c98cf47f4dc51e55c9
Roberto Sousa <robertosousa186@gmail.com>
2021-07-30 02:14:08 -0300

    Fix user model

--- src/user/User.js
+++ src/user/User.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 const sequelize = require('../config/database');
 
-const Model = sequelize.Model;
+const Model = Sequelize.Model;
 /*
  * Models are the essence of Sequelize. A model is an abstraction that represents a table in your database.
  * In Sequelize, it is a class that extends Model.
