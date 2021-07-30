e8deb8afc66e90f3689b4531988d9b4bd7f44ee2
Roberto Sousa <robertosousa186@gmail.com>
2021-07-30 01:11:51 -0300

    Creating a database connection functionality

---
+++ src/config/database.js
@@ -0,0 +1,9 @@
+const Sequelize = require('sequelize');
+
+// Creating a new instance of sequelize.
+const sequelize = new Sequelize('hoaxify', 'my-db-user', 'db-pass', {
+  dialect: 'sqlite',
+  storage: './database.sqlite',
+});
+
+module.exports = sequelize;
