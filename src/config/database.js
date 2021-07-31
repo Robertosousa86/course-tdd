9f4393c88682c67faebec5fabeb7b19e99b38009
Roberto Sousa <robertosousa186@gmail.com>
2021-07-31 01:47:35 -0300

    Adding 'logging: false' to clean-up message log in tests

--- src/config/database.js
+++ src/config/database.js
@@ -4,6 +4,7 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize('hoaxify', 'my-db-user', 'db-pass', {
   dialect: 'sqlite',
   storage: './database.sqlite',
+  logging: false,
 });
 
 module.exports = sequelize;
