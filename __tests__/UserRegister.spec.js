a27867e0fb8cd662fb6db2cf023cac4f3ad6f003
Roberto Sousa <robertosousa186@gmail.com>
2021-07-31 01:41:58 -0300

    Creating test for validation of username and email

--- __tests__/UserRegister.spec.js
+++ __tests__/UserRegister.spec.js
@@ -60,4 +60,23 @@ describe('User Registration', () => {
         });
       });
   });
+
+  it('should be saves the username and email to database', (done) => {
+    request(app)
+      .post('/api/1.0/users')
+      .send({
+        username: 'user1',
+        email: 'user1@gmail.com',
+        password: 'P4ssword',
+      })
+      .then(() => {
+        // Query user
+        User.findAll().then((userList) => {
+          const savedUser = userList[0];
+          expect(savedUser.username).toBe('user1');
+          expect(savedUser.email).toBe('user1@gmail.com');
+          done();
+        });
+      });
+  });
 });
