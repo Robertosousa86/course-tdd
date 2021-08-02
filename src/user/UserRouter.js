9cb54bb6bb2dc648c5ea00b561ef5d6f4cae13a0
Roberto Sousa <robertosousa186@gmail.com>
2021-08-02 01:55:14 -0300

    Refactoring: Creating bussines logic

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -1,13 +1,10 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
-const User = require('./User');
+const UserService = require('./UserService');
 
 const router = express.Router();
 
 router.post('/api/1.0/users', async (req, res) => {
-  const hash = await bcrypt.hash(req.body.password, 10);
-  const user = { ...req.body, password: hash };
-  await User.create(user);
+  await UserService.save(req.body);
   return res.send({ message: 'User created.' });
 });
 
