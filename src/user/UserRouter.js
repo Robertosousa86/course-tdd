dd2f2a80036b82c6f3912b07a2addf7373ff1b22
Roberto Sousa <robertosousa186@gmail.com>
2021-08-02 01:42:11 -0300

    Refactoring: Using async await

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -4,13 +4,11 @@ const User = require('./User');
 
 const router = express.Router();
 
-router.post('/api/1.0/users', (req, res) => {
-  bcrypt.hash(req.body.password, 10).then((hash) => {
-    const user = { ...req.body, password: hash };
-    User.create(user).then(() => {
-      return res.send({ message: 'User created.' });
-    });
-  });
+router.post('/api/1.0/users', async (req, res) => {
+  const hash = await bcrypt.hash(req.body.password, 10);
+  const user = { ...req.body, password: hash };
+  await User.create(user);
+  return res.send({ message: 'User created.' });
 });
 
 module.exports = router;
