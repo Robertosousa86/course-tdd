4fa1fd3c5991eb9b8d65100e6c22492273ba53d8
Roberto Sousa <robertosousa186@gmail.com>
2021-08-03 01:18:34 -0300

    Updating database for use environment variable javascript object

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,13 @@
 const Sequelize = require('sequelize');
+const config = require('config');
+
+const dbConfig = config.get('database');
 
 // Creating a new instance of sequelize.
-const sequelize = new Sequelize('hoaxify', 'my-db-user', 'db-pass', {
-  dialect: 'sqlite',
-  storage: './database.sqlite',
-  logging: false,
+const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
+  dialect: dbConfig.dialect,
+  storage: dbConfig.storage,
+  logging: dbConfig.logging,
 });
 
 module.exports = sequelize;
