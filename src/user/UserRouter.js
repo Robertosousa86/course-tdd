ea81051abc452544ed209d2d69de9c6473a10753
Juliana Lima <julianalimail@gmail.com>
2021-08-04 00:11:14 -0300

    Implamenting validating user

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -4,6 +4,10 @@ const UserService = require('./UserService');
 const router = express.Router();
 
 router.post('/api/1.0/users', async (req, res) => {
+  const user = req.body;
+
+  if (user.username === null) return res.status(400).send();
+
   await UserService.save(req.body);
   return res.send({ message: 'User created.' });
 });
