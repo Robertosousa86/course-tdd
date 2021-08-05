29d995d12ff9606c3ec8af749861437b99c665d8
Roberto Sousa <robertosousa186@gmail.com>
2021-08-05 01:15:50 -0300

    Using validator middleware

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -3,18 +3,23 @@ const UserService = require('./UserService');
 
 const router = express.Router();
 
-router.post('/api/1.0/users', async (req, res) => {
-  const user = req.body;
+router.post(
+  '/api/1.0/users',
+  (req, res, next) => {
+    const user = req.body;
 
-  if (user.username === null)
-    return res.status(400).send({
-      validationErrors: {
-        username: 'Username cannot be null',
-      },
-    });
-
-  await UserService.save(req.body);
-  return res.send({ message: 'User created.' });
-});
+    if (user.username === null)
+      return res.status(400).send({
+        validationErrors: {
+          username: 'Username cannot be null',
+        },
+      });
+    next();
+  },
+  async (req, res) => {
+    await UserService.save(req.body);
+    return res.send({ message: 'User created.' });
+  }
+);
 
 module.exports = router;
