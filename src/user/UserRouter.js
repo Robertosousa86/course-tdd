8a735cad72812cf61bed34b48f9c3c0ac7838578
Roberto Sousa <robertosousa186@gmail.com>
2021-08-05 03:01:23 -0300

    Implementing middleware for validate username and email

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -7,11 +7,9 @@ const validateUsername = (req, res, next) => {
   const user = req.body;
 
   if (user.username === null)
-    return res.status(400).send({
-      validationErrors: {
-        username: 'Username cannot be null',
-      },
-    });
+    req.validationErrors = {
+      username: 'Username cannot be null',
+    };
   next();
 };
 
@@ -19,15 +17,18 @@ const validateEmail = (req, res, next) => {
   const user = req.body;
 
   if (user.email === null)
-    return res.status(400).send({
-      validationErrors: {
-        email: 'E-mail cannot be null',
-      },
-    });
+    req.validationErrors = {
+      ...req.validationErrors,
+      email: 'E-mail cannot be null',
+    };
   next();
 };
 
 router.post('/api/1.0/users', validateUsername, validateEmail, async (req, res) => {
+  if (req.validationErrors) {
+    const response = { validationErrors: { ...req.validationErrors } };
+    return res.status(400).send(response);
+  }
   await UserService.save(req.body);
   return res.send({ message: 'User created.' });
 });
