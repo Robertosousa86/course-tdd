57b4b96855d0a329110aff7dc8417dfdf2aa47aa
Roberto Sousa <robertosousa186@gmail.com>
2021-08-05 02:18:57 -0300

    Creating test for both when username and email are null

--- __tests__/UserRegister.spec.js
+++ __tests__/UserRegister.spec.js
@@ -97,4 +97,14 @@ describe('User Registration', () => {
     const body = response.body;
     expect(body.validationErrors.email).toBe('E-mail cannot be null');
   });
+
+  it('should be returns errors for both when username and email is null', async () => {
+    const response = await postUser({
+      username: null,
+      email: null,
+      password: 'P4ssword',
+    });
+    const body = response.body;
+    expect(Object.keys(body.validationErrors)).toEqual(['username', 'email']);
+  });
 });
