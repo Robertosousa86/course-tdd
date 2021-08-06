d02f898f993722a301be65c208741a4988bf5dc4
Roberto Sousa <robertosousa186@gmail.com>
2021-08-06 02:46:23 -0300

    Refactoring: Using dynamic tests

--- __tests__/UserRegister.spec.js
+++ __tests__/UserRegister.spec.js
@@ -78,26 +78,6 @@ describe('User Registration', () => {
     expect(body.validationErrors).not.toBeUndefined();
   });
 
-  it('should be returns Username cannot be null when username is null', async () => {
-    const response = await postUser({
-      username: null,
-      email: 'user1@mail.com',
-      password: 'P4ssword',
-    });
-    const body = response.body;
-    expect(body.validationErrors.username).toBe('Username cannot be null');
-  });
-
-  it('should be returns E-mail cannot be null when email is null', async () => {
-    const response = await postUser({
-      username: 'user1',
-      email: null,
-      password: 'P4ssword',
-    });
-    const body = response.body;
-    expect(body.validationErrors.email).toBe('E-mail cannot be null');
-  });
-
   it('should be returns errors for both when username and email is null', async () => {
     const response = await postUser({
       username: null,
@@ -108,13 +88,19 @@ describe('User Registration', () => {
     expect(Object.keys(body.validationErrors)).toEqual(['username', 'email']);
   });
 
-  it('should be returns Password cannot be null', async () => {
-    const response = await postUser({
+  it.each([
+    ['username', 'Username cannot be null'],
+    ['email', 'E-mail cannot be null'],
+    ['password', 'Password cannot be null'],
+  ])('when %s is null %s is received', async (field, expectedMessage) => {
+    const user = {
       username: 'user1',
       email: 'user1@mail.com',
-      password: null,
-    });
+      password: 'P4ssword',
+    };
+    user[field] = null;
+    const response = await postUser(user);
     const body = response.body;
-    expect(body.validationErrors.password).toBe('Password cannot be null');
+    expect(body.validationErrors[field]).toBe(expectedMessage);
   });
 });
