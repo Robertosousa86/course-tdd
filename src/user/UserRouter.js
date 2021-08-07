b422fe777d8f29e47fad11245c22eaf5d7950069
Roberto Sousa <robertosousa186@gmail.com>
2021-08-07 00:57:17 -0300

    First implementation

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -6,7 +6,11 @@ const router = express.Router();
 
 router.post(
   '/api/1.0/users',
-  check('username').notEmpty().withMessage('Username cannot be null'),
+  check('username')
+    .isLength({ min: 4 })
+    .withMessage('Must have min 4 and max 32 characters')
+    .notEmpty()
+    .withMessage('Username cannot be null'),
   check('email').notEmpty().withMessage('E-mail cannot be null'),
   check('password').notEmpty().withMessage('Password cannot be null'),
   async (req, res) => {
