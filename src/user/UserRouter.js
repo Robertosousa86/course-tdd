9844bb352c8bd81dcfbe777e508c98880e2c51b4
Roberto Sousa <robertosousa186@gmail.com>
2021-08-18 23:58:15 -0300

    Using variables

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -8,30 +8,30 @@ router.post(
   '/api/1.0/users',
   check('username')
     .notEmpty()
-    .withMessage('Username cannot be null')
+    .withMessage('username_null')
     .bail()
     .isLength({ min: 4, max: 32 })
-    .withMessage('Must have min 4 and max 32 characters'),
+    .withMessage('username_size'),
   check('email')
     .notEmpty()
-    .withMessage('E-mail cannot be null')
+    .withMessage('email_null')
     .bail()
     .isEmail()
-    .withMessage('E-mail is not valid')
+    .withMessage('email_invalid')
     .bail()
     .custom(async (email) => {
       const user = await UserService.findByEmail(email);
-      if (user) throw new Error('E-mail in use');
+      if (user) throw new Error('email_inuse');
     }),
   check('password')
     .notEmpty()
-    .withMessage('Password cannot be null')
+    .withMessage('password_null')
     .bail()
     .isLength({ min: 6 })
-    .withMessage('Password must be at least 6 characters')
+    .withMessage('Password_size')
     .bail()
     .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).*$/)
-    .withMessage('Password must have at least 1 uppercase, 1 lowercase letter and 1 number'),
+    .withMessage('password-pattern'),
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
