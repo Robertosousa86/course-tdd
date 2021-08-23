6794644a22972083e3e1d0d151894fd62d93d568
Roberto Sousa <robertosousa186@gmail.com>
2021-08-23 00:49:10 -0300

    Creating function generateToken with native library crypto

--- src/user/UserService.js
+++ src/user/UserService.js
@@ -1,5 +1,11 @@
 const bcrypt = require('bcrypt');
 const User = require('./User');
+// crypto is a native library of nodejs for cryptography
+const crypto = require('crypto');
+
+const generateToken = (length) => {
+  return crypto.randomBytes(length).toString('hex').substring(0, length);
+};
 
 const save = async (body) => {
   const { username, email, password } = body;
