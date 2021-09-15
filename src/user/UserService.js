d72f868326310d64dc500cb0f3044a95d9d24a49
Roberto Sousa <robertosousa186@gmail.com>
2021-09-15 00:29:13 -0300

    Implementing 'send account validation email' functionality

--- src/user/UserService.js
+++ src/user/UserService.js
@@ -2,6 +2,8 @@ const bcrypt = require('bcrypt');
 const User = require('./User');
 // crypto is a native library of nodejs for cryptography
 const crypto = require('crypto');
+const nodemailer = require('nodemailer');
+const nodemailerStub = require('nodemailer-stub');
 
 const generateToken = (length) => {
   return crypto.randomBytes(length).toString('hex').substring(0, length);
@@ -12,6 +14,13 @@ const save = async (body) => {
   const hash = await bcrypt.hash(password, 10);
   const user = { username: username, email: email, password: hash, activationToken: generateToken(16) };
   await User.create(user);
+  const transporter = nodemailer.createTransport(nodemailerStub.stubTransport);
+  await transporter.sendMail({
+    from: 'My app <info@my-app.com',
+    to: email,
+    subject: 'Account activation',
+    html: `Your token is ${user.activationToken}`,
+  });
 };
 
 const findByEmail = async (email) => {
