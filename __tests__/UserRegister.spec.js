3cb112e0f274a070657ddc00897352be7fcf4219
Roberto Sousa <robertosousa186@gmail.com>
2021-09-15 00:22:51 -0300

    Fix the test case

--- __tests__/UserRegister.spec.js
+++ __tests__/UserRegister.spec.js
@@ -114,15 +114,15 @@ describe('User Registration', () => {
     const users = await User.findAll();
     const savedUser = users[0];
     expect(savedUser.activationToken).toBeTruthy();
+  });
 
-    it('should sends Account activation email with activationToken', async () => {
-      await postUser();
-      const lastMail = nodemailerStub.interactsWithMail.lastMail();
-      expect(lastMail.to[0]).toBe('user1@mail.com');
-      const users = await User.findAll();
-      const savedUser = users[0];
-      expect(lastMail.content).toContain(savedUser.activationToken);
-    });
+  it('should sends Account activation email with activationToken', async () => {
+    await postUser();
+    const lastMail = nodemailerStub.interactsWithMail.lastMail();
+    expect(lastMail.to[0]).toBe('user1@mail.com');
+    const users = await User.findAll();
+    const savedUser = users[0];
+    expect(lastMail.content).toContain(savedUser.activationToken);
   });
 
   const username_null = 'Username cannot be null';
